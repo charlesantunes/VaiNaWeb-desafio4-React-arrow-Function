a2a6a756d7e6bee14221cc7077aedc2188feec8c
charlesantunes <charleswcantunes@gmail.com>
2022-07-14 02:52:48 -0300

    final 4 desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  msg = () => {
+    return <h2> Msg de função</h2>
+  }
+
+  dobro = (num) => {
+    return  num * 2
+  }
+
+  render(){
+    return(
+      <>
+      <h1>Olá</h1>
+      {this.msg()}
+      <h2>{"O dobro de 4 é " + this.dobro(4)}</h2>
+      </>
+    )
+  }
 }
-
 export default App;
